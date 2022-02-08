adcf94b296a5e71696e85e0281f76db7274711e3
Doug Leung <dleung46@mylangara.ca>
2022-02-08 14:40:58 -0800

    Feat: To set up folder structure and basic files

--- client/App.js
+++ client/App.js
@@ -1,20 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { NativeBaseProvider } from 'native-base'
+import AppStack from '../client/src/components/stacks/AppStack/AppStack.js'
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NativeBaseProvider>
+      <AppStack />
+      <StatusBar style='light' />
+    </NativeBaseProvider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
+
---
+++ client/src/components/stacks/AppStack/screens/IndexScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, Center, Box } from 'native-base';
+
+const IndexScreen = () => {
+    return (
+        <>
+        <Center>
+            <Box>
+                <Text fontSize={25}>Welcome to Parkr</Text>
+            </Box>
+        </Center>
+        </>
+    )
+} 
+
+export default IndexScreen;
