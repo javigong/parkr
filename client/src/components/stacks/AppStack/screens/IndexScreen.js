de7b0c9f89d1a8a6a6d8453abf7b87557e795704
Doug Leung <84534062+dougieleung@users.noreply.github.com>
2022-02-10 09:32:33 -0800

    feat: just added a login and sign up button on the index screen so we can test authentication (#2)
    * feat: to add login/signup buttons to index screen
    
    * feat: to add login/sign up buttons for auth

--- client/src/components/stacks/AppStack/screens/IndexScreen.js
+++ client/src/components/stacks/AppStack/screens/IndexScreen.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Text, Center, Box } from 'native-base';
+import { Text, Center, Box, VStack, Button } from 'native-base';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
     return (
         <>
         <Center>
             <Box>
+              <VStack flex={1} space={4} justifyContent="center"> 
                 <Text fontSize={25}>Welcome to Parkr</Text>
+                <Button py={3} px={1} size="lg" backgroundColor="emerald.600" onPress={() => navigation.navigate("AuthStack", {screen: "Login"})}>Login</Button>
+                <Button py={3} px={1} size="lg" backgroundColor="emerald.600" onPress={() => navigation.navigate("AuthStack", {screen: "Signup"})}>Signup</Button>
+              </VStack>   
             </Box>
         </Center>
         </>
