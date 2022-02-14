6fec7bb03f20ace32ae5bf9138c12bdd3a1208c3
Yulia <mail4yulia@yahoo.com>
2022-02-13 17:17:21 -0800

    push additional code/files to complete commit

---
+++ client/src/components/stacks/AuthStack/screens/ParkingLocation.js
@@ -0,0 +1,11 @@
+import { Box, Input } from "native-base";
+
+const ParkingLocationInput = () => {
+  return (
+    <Box alignItems="center">
+      <Input mx="3" placeholder="Input" w="75%" maxWidth="300px" />
+    </Box>
+  );
+};
+
+export default ParkingLocationInput;
