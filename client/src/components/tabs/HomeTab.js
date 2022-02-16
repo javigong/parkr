d1a8c61d55a5e0a7faee4d709b7aea6ed1e7c104
Javier Gongora <jgongora@gmail.com>
2022-02-16 14:33:40 -0800

    set bottom tab structure

--- client/src/components/tabs/HomeTab.js
+++ client/src/components/tabs/HomeTab.js
@@ -1,4 +1,5 @@
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 import { auth } from "../config/firebase";
 import ParkingScreen from "../screens/home-tab/ParkingScreen";
 
@@ -11,18 +12,73 @@ const HomeTab = () => {
 
   return (
     <Tab.Navigator
-    options={{
-          headerRight: () => (
-            <Button onPress={onSignOut} title="Logout" color="#000" />
-          ),
-        }}>
+      initialRouteName="Park"
+      initialRouteName="Feed"
+      activeColor="black"
+      labelStyle={{ fontSize: 12 }}
+      style={{ backgroundColor: "rgb(94,39,161)" }}
+      options={{
+        headerRight: () => (
+          <Button onPress={onSignOut} title="Logout" color="rgb(94,39,161)" />
+        ),
+      }}
+    >
       <Tab.Screen
         name="Parking"
         component={ParkingScreen}
+        options={{
+          tabBarLabel: "Park",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="local-parking"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
       />
-      {/* <Tab.Screen name="Host" component={HostScreen} />
-      <Tab.Screen name="Chat" component={ChatScreen} />
-      <Tab.Screen name="Account" component={Account} /> */}
+      <Tab.Screen
+        name="Host"
+        component={HostScreen}
+        options={{
+          tabBarLabel: "Park",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="local-parking"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Chat"
+        component={ChatScreen}
+        options={{
+          tabBarLabel: "Park",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="local-parking"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Account"
+        component={Account}
+        options={{
+          tabBarLabel: "Park",
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons
+              name="local-parking"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
