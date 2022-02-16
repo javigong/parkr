007f4029ca79037c1ededfdecf31fbf23f2284e4
Yulia <mail4yulia@yahoo.com>
2022-02-15 19:50:21 -0800

    delete authstack folder from stacks

--- client/src/components/screens/auth-stack/ParkingLocation.js
+++ client/src/components/screens/auth-stack/ParkingLocation.js
@@ -1,9 +1,13 @@
-import { Box, Input } from "native-base";
+import { Box, Input, Button } from "native-base";
+import { Autocomplete, Button } from "react-native";
 
 const ParkingLocationInput = () => {
   return (
     <Box alignItems="center">
       <Input mx="3" placeholder="Input" w="75%" maxWidth="300px" />
+      <Autocomplete />
+
+      <Button />
     </Box>
   );
 };
