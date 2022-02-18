fe8730f03aba37e4d401e5be0f64e7b7564cf29d
Mark Joseph Suria <84940087+mark-suria@users.noreply.github.com>
2022-02-18 14:42:26 -0800

    Add: Backend Nodejs Structure (#27)
    * update firebase config
    
    * change tree folder
    
    * fix login
    
    * Fix: using android emulator
    
    * Backend Setup
    
    Setup MySQL database using SSH to connect
    
    * Setup:ServerConfiguration
    
    * Fix:WebServer MySQL Docker
    
    * fix: delete duplicated client .env
    
    * clean tree folder
    
    Co-authored-by: Javier Gongora <jgongora@gmail.com>

--- client/src/components/config/firebase.js
+++ client/src/components/config/firebase.js
@@ -5,13 +5,13 @@ import Constants from 'expo-constants';
 
 // firebase config
 const firebaseConfig = {
-  apiKey: Constants.manifest.extra.apiKey,
-  authDomain: Constants.manifest.extra.authDomain,
-  projectId: Constants.manifest.extra.projectId,
-  storageBucket: Constants.manifest.extra.storageBucket,
-  messagingSenderId: Constants.manifest.extra.messagingSenderId,
-  appId: Constants.manifest.extra.appId,
-  databaseURL: Constants.manifest.extra.databaseURL
+    apiKey: Constants.manifest.extra.apiKey,
+    authDomain: Constants.manifest.extra.authDomain,
+    projectId: Constants.manifest.extra.projectId,
+    storageBucket: Constants.manifest.extra.storageBucket,
+    messagingSenderId: Constants.manifest.extra.messagingSenderId,
+    appId: Constants.manifest.extra.appId,
+    databaseURL: Constants.manifest.extra.databaseURL
 };
 
 // initialize firebase
---
+++ server/app.js
@@ -0,0 +1,27 @@
+const { Sequelize } = require('sequelize');
+const tunnel = require('tunnel-ssh');
+
+const environment = process.env.ENV || 'development';
+const config = require('./config/config.js')[environment];
+
+const { host, port, user, password, database } = config.database;
+
+const sequelize = new Sequelize(database, user, password, {
+    dialect: 'mysql',
+    logging: false
+});
+
+const configTunnelSSH = config.tunnelSSH;
+
+tunnel(configTunnelSSH, (error, server) => {
+    if (error) {
+        console.error(error);
+    } else {
+        console.info('Server Info: ', server);
+        sequelize.authenticate().then(() => {
+            console.info('Connection established');
+        }).catch((err) => {
+            console.error('Unable to establish connection', err);
+        });
+    }
+});
