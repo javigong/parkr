c9e4c7d9def409e69e583b15d785aa5673718a2c
Javier Gongora <jgongora@gmail.com>
2022-02-28 14:54:25 -0800

    Add Log Out button to AccountScreen

--- client/src/components/screens/home-tab/AccountScreen.js
+++ client/src/components/screens/home-tab/AccountScreen.js
@@ -1,20 +1,50 @@
 import React from "react";
-import { Center, Text } from "native-base";
+import { Box, Button, Center, Text, VStack } from "native-base";
+import { StyleSheet } from "react-native";
+import { signOut } from "firebase/auth";
+import { auth } from "../../config/firebase";
 
 const AccountScreen = () => {
+  const onSignOut = () => {
+    signOut(auth).catch((error) => console.log("Error logging out: ", error));
+  };
+
   return (
-    <Center>
-      <Center bg="primary.400" _text={{
-      color: "white",
-      fontWeight: "bold"
-    }} height={200} width={{
-      base: 200,
-      lg: 255
-    }}>
-        AccountScreen
-      </Center>
+    <Center flex={1} w="100%">
+      <Box safeArea p="2" py="8" w="90%">
+        <VStack>
+          <Button
+            colorScheme="rgb(253,107,54)"
+            height={42}
+            width="100%"
+            mt="2"
+            size="lg"
+            borderRadius={20}
+            onPress={onSignOut}
+          >
+            <Text color="white" fontWeight="bold">
+              Log Out
+            </Text>
+          </Button>
+        </VStack>
+      </Box>
     </Center>
   );
 };
 
 export default AccountScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#FD6B36",
+  },
+  tabContent: {
+    textAlign: "center",
+    height: "100%",
+    color: "black",
+    backgroundColor: "white",
+    fontSize: 18,
+  },
+});
