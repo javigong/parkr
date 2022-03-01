34dadcefc6a298dc43d4c4fe31b8a05fcc56ea90
Javier Gongora <jgongora@gmail.com>
2022-02-28 20:02:36 -0800

    feat: add ParkingSpotCard
    It can be used for different FlatLists, for spots available only for the same day or for a period with start and end date/time. Includes different types of parking (motorcycle, ev charger, standard), owner initials, spot number, and price.

--- client/src/components/cards/ParkingSpotCard.js
+++ client/src/components/cards/ParkingSpotCard.js
@@ -1,21 +1,14 @@
-import { Badge, Box, Center, Divider, HStack, Text, VStack } from "native-base";
+import { Box, Center, Divider, HStack, Text, VStack } from "native-base";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const ParkingSpotCard = ({
-  ownerInitials = "FH",
-  spotNumber = "10",
-  type = "standard",
-  startDate = "Feb 20",
-  startTime = "06:00",
-  endDate = "Feb 21",
-  endTime = "17:00",
-  price = "0.00",
-}) => {
+const ParkingSpotCard = ({ item }) => {
+
   return (
-    <SafeAreaView>
+    <>
       <Box
-        m="4"
+        mx="4"
+        my="1"
         px="4"
         py="2"
         borderWidth="1"
@@ -33,16 +26,16 @@ const ParkingSpotCard = ({
             >
               <Center h="39">
                 <Text color="white" fontSize="xl">
-                  {ownerInitials}
+                  {item.ownerInitials}
                 </Text>
               </Center>
             </Box>
             <VStack space="1">
               <Box>
-                <Text fontSize="xl">Spot {spotNumber}</Text>
+                <Text fontSize="xl">Spot {item.spotNumber}</Text>
               </Box>
               <Box>
-                {type == "ev" && (
+                {item.type == "ev" && (
                   <Box
                     px="2"
                     borderWidth="1"
@@ -54,7 +47,7 @@ const ParkingSpotCard = ({
                     </Text>
                   </Box>
                 )}
-                {type == "motorcycle" && (
+                {item.type == "motorcycle" && (
                   <Box
                     px="2"
                     borderWidth="1"
@@ -66,7 +59,7 @@ const ParkingSpotCard = ({
                     </Text>
                   </Box>
                 )}
-                {type == "standard" && (
+                {item.type == "standard" && (
                   <Box
                     px="2"
                     borderWidth="1"
@@ -84,16 +77,16 @@ const ParkingSpotCard = ({
           <HStack space="4" justifyContent="space-between">
             <Box>
               <Text>
-                {startDate}, {startTime} - {endDate}, {endTime}
+                {item.startDate}, {item.startTime} {item.endDate && `- ${item.endDate}, ${item.endTime}`}
               </Text>
             </Box>
             <Box>
-              <Text>CAD ${price}</Text>
+              <Text>CAD ${item.price}</Text>
             </Box>
           </HStack>
         </VStack>
       </Box>
-    </SafeAreaView>
+    </>
   );
 };
 
