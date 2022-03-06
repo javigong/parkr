315afc574129f6d1066b621deec544b1c6bb6b26
Yulia <mail4yulia@yahoo.com>
2022-03-05 23:30:02 -0800

    add dae and time picker separate

--- client/src/components/screens/parking-stack/FindParkingScreen.js
+++ client/src/components/screens/parking-stack/FindParkingScreen.js
@@ -1,8 +1,33 @@
-import React from "react";
-import { Box, Text, Input, HStack } from "native-base";
+import React, { useState } from "react";
+import { Button } from "react-native";
+import { Box, Text, View, Input, HStack } from "native-base";
 import { Svg, Path } from "react-native-svg";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 const FindParkingScreen = () => {
+  const [date, setDate] = useState(new Date(1598051730000));
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setShow(Platform.OS === "ios");
+    setDate(currentDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode("date");
+  };
+
+  const showTimepicker = () => {
+    showMode("time");
+  };
+
   return (
     <Box>
       <Text>Select the period for parking</Text>
@@ -20,7 +45,40 @@ const FindParkingScreen = () => {
             fill="#FD6B36"
           />
         </Svg>
-        <Input variant="underlined"></Input>
+        <Text>Start</Text>
+      </HStack>
+      <View>
+        <View>
+          <Button onPress={showDatepicker} title="Show date picker!" />
+        </View>
+        <View>
+          <Button onPress={showTimepicker} title="Show time picker!" />
+        </View>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={true}
+            display="default"
+            onChange={onChange}
+          />
+        )}
+      </View>
+      <HStack>
+        <Svg
+          width="26"
+          height="27"
+          viewBox="0 0 26 27"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <Path
+            d="M13.0353 24.9999L24.4753 26.5682L20.1135 15.8768L13.0353 24.9999ZM2.00006 13.0324C1.98217 6.95725 6.89252 2.01791 12.9676 2.00002L12.9617 3.15271e-05C5.78207 0.0211657 -0.0210702 5.85857 6.39595e-05 13.0382L2.00006 13.0324ZM12.9676 2.00002C19.0427 1.98214 23.9821 6.89249 24 12.9676L26 12.9617C25.9788 5.78204 20.1414 -0.0211026 12.9617 3.15271e-05L12.9676 2.00002ZM24 12.9676C24.0091 16.0709 22.7336 18.8763 20.6714 20.8835L22.0664 22.3167C24.5008 19.9473 26.0107 16.6289 26 12.9617L24 12.9676Z"
+            fill="#FD6B36"
+          />
+        </Svg>
+        <Text>End</Text>
       </HStack>
     </Box>
   );
