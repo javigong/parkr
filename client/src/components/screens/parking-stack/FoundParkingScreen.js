d7dc5edd5c4c2dbf19c0b9f258ba77bc577d6070
Yulia <mail4yulia@yahoo.com>
2022-03-12 22:37:14 -0800

    FoundParkingScreen add

---
+++ client/src/components/screens/parking-stack/FoundParkingScreen.js
@@ -0,0 +1,16 @@
+import { Box } from "native-base";
+
+const FoundParkingScreen = (navigation, route) => {
+  const { startDate, endDate } = route.params;
+
+  return (
+    <Box>
+      <Text>Results</Text>
+      <Text>
+        {startDate} - {endDate}
+      </Text>
+    </Box>
+  );
+};
+
+export default FoundParkingScreen;
