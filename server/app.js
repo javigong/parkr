e50cb1874ded5dd8f8abb5da98c2dabeb662a71a
Javier Gongora <jgongora@gmail.com>
2022-03-14 13:14:20 -0700

    add firebase-admin and google application credentials

--- server/app.js
+++ server/app.js
@@ -8,9 +8,6 @@ app.use(cors({
 }));
 const { initializeApp } = require('firebase-admin/app');
 const admin = require('firebase-admin');
-const googleAppCredentials = require('./config/google_app_credentials.json');
-
-const { initializeApp } = require('firebase-admin/app');
 
 //const cookieParser = require("cookie-parser");
 const PORT = process.env.PORT || 4000;
@@ -33,12 +30,12 @@ app.use("/", router);
 app.listen(PORT, () => console.log(`Server is starting at ${PORT}`));
 
 //firebase admin sdk
-initializeApp({
-    credential: admin.credential.cert(googleAppCredentials),
-    apiKey: "AIzaSyAIYaUqhUiWor-4am_U8Vs4A7dry_5Q2F0",
-    authDomain: "parkr-auth.firebaseapp.com",
-    projectId: "parkr-auth",
-    storageBucket: "parkr-auth.appspot.com",
-    messagingSenderId: "1013744309561",
-    appId: "1:1013744309561:web:66f2970dd44d6eb47144e8"
-});
+// initializeApp({
+//     credential: admin.credential.applicationDefault(),
+//     apiKey: "AIzaSyAIYaUqhUiWor-4am_U8Vs4A7dry_5Q2F0",
+//     authDomain: "parkr-auth.firebaseapp.com",
+//     projectId: "parkr-auth",
+//     storageBucket: "parkr-auth.appspot.com",
+//     messagingSenderId: "1013744309561",
+//     appId: "1:1013744309561:web:66f2970dd44d6eb47144e8"
+// });
