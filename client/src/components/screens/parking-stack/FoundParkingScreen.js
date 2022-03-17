b3a6c1dddc6aa024552757ad53e19299028acbd7
Yulia <mail4yulia@yahoo.com>
2022-03-16 21:43:35 -0700

    useEffect, pulling data from DB not working

--- client/src/components/screens/parking-stack/FoundParkingScreen.js
+++ client/src/components/screens/parking-stack/FoundParkingScreen.js
@@ -1,15 +1,24 @@
-import { Box, Text } from "native-base";
-import React, { useEffect, useState } from "react";
+import { Box, Text, View } from "native-base";
+import React, { useEffect, useState, useContext } from "react";
 import { format } from "date-fns";
 import { getAllParkingSpots } from "../../services/api";
 import ParkingSpotList from "../../lists/ParkingSpotList";
+import { AuthenticatedUserContext } from "../../contexts/AuthenticatedUserContext";
 
 const FoundParkingScreen = ({ navigation, route }) => {
-  const { startDate, endDate } = route.params;
-  const [filteredSpotsList, setFilteredSpotsList] = useState(null);
+  const { startDate, endDate, currentDate } = route.params;
+  const [filteredSpotsList, setFilteredSpotsList] = useState();
+  const { user, setUser } = useContext(AuthenticatedUserContext);
 
   useEffect(() => {
-    getAllParkingSpots().then((results) => setFilteredSpotsList(results));
+    const tokenJwt = user.accessToken;
+    // const date = new Date();
+    // setCurrentDate(date.toString().slice(4, 10));
+    getAllParkingSpots(tokenJwt).then(
+      (results) => setFilteredSpotsList(results)
+      // console.log(results)
+    );
+    console.log(filteredSpotsList);
   }, []);
 
   return (
@@ -19,7 +28,15 @@ const FoundParkingScreen = ({ navigation, route }) => {
         {format(startDate, "EEE,d MMM, hh:mm b ")} - {""}
         {format(endDate, "EEE,d MMM, hh:mm b ")}
       </Text>
-      {() => <ParkingSpotList data={filteredSpotsList} />}
+
+      {() => (
+        <ParkingSpotList
+          data={filteredSpotsList}
+          type="searchResults"
+          currentDate={currentDate}
+          navigation={navigation}
+        />
+      )}
     </Box>
   );
 };
