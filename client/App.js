5f79beaa98afe5e10542c8a7205f455efd6962e4
Doug Leung <dleung46@mylangara.ca>
2022-03-16 22:24:44 -0700

    fix: switch back to main parking screen

--- client/App.js
+++ client/App.js
@@ -57,13 +57,13 @@ function RootNavigator() {
     return <Loading />;
   }
 
-  // return <>{user ? <AppStack /> : <AuthStack />}</>;
+  return <>{user ? <AppStack /> : <AuthStack />}</>;
 
   // return <AuthStack />;
   // return <WelcomeScreen />;
   // return <SignupFormScreen />;
   // return <AppStack />;
-  return <LicensePlateScreen />;
+  // return <LicensePlateScreen />;
   // return <WhoParksScreen />;
   // return <ChooseCarScreen />;
   // return <ConfirmDateTimeScreen />;
