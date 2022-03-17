3dda6d79f913563b93b78ea9cef2d9c67d867d15
Javier Gongora <jgongora@gmail.com>
2022-03-17 00:26:28 -0700

    Update ChatStack.js

--- client/src/components/stacks/ChatStack.js
+++ client/src/components/stacks/ChatStack.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LiveChatScreen from "../screens/chat-stack/_LiveChatScreen";
+import LiveChatScreen from "../screens/chat-stack/LiveChatScreen";
 
 const Stack = createNativeStackNavigator();
 
 const ChatStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="LiveChatScreen" component={LiveChatScreen} />
+      <Stack.Screen
+        name="LiveChatScreen"
+        component={LiveChatScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 };
