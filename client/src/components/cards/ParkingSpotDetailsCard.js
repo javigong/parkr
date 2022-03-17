81152125744aa2b5d281639fbb2b32dde6a9402b
Javier Gongora <jgongora@gmail.com>
2022-03-17 00:26:21 -0700

    Update ParkingSpotDetailsCard.js

--- client/src/components/cards/ParkingSpotDetailsCard.js
+++ client/src/components/cards/ParkingSpotDetailsCard.js
@@ -2,9 +2,16 @@ import React from "react";
 import { Box, Button, Text, VStack, HStack, Icon } from "native-base";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons } from "@expo/vector-icons";
+import { updateCurrentUser } from "firebase/auth";
 
 const ParkingSpotDetailsCard = ({ item, currentDate, type, navigation }) => {
-  console.log(type);
+  const ChatHandler = () => {
+    navigation.navigate("ChatStack", {
+      screen: "LiveChatScreen",
+      params: { item: item, currentDate: currentDate, type: type },
+    });
+  };
+
   return (
     <SafeAreaView flex="1" alignItems="center" backgroundColor="white">
       <Box flex="1" width="80%" justifyContent="space-between" mt={6}>
@@ -26,8 +33,10 @@ const ParkingSpotDetailsCard = ({ item, currentDate, type, navigation }) => {
               </HStack>
               <HStack mb={3}>
                 <Text fontWeight="bold">End</Text>
-                <Text color="grey">{"   "}
-                  {currentDate}, {item.rsrv_end.slice(0, 5)}</Text>
+                <Text color="grey">
+                  {"   "}
+                  {currentDate}, {item.rsrv_end.slice(0, 5)}
+                </Text>
               </HStack>
             </Box>
             <Box borderBottomWidth={1} borderBottomColor="#FD6B36">
@@ -35,17 +44,22 @@ const ParkingSpotDetailsCard = ({ item, currentDate, type, navigation }) => {
                 <Text fontSize={16} fontWeight="bold">
                   Who's Parking
                 </Text>
-                <Text>{}</Text>
+                <Text>
+                  {item.paVisitorId
+                    ? `${item.upFirstName} ${item.upLastName}`
+                    : "Visitor"}
+                </Text>
               </VStack>
             </Box>
             <Box borderBottomWidth={1} borderBottomColor="#FD6B36">
               <VStack space={2} mb={3}>
                 <Text fontSize={16} fontWeight="bold">
-                  Payment
+                  Fee
                 </Text>
                 <HStack justifyContent="space-between">
-                  <Text>CAD $1.50 total</Text>
-                  <Text>Free</Text>
+                  <Text>
+                    {item.paFee ? `CAD $${item.paFee} x hour` : `Free`}
+                  </Text>
                 </HStack>
               </VStack>
             </Box>
@@ -54,20 +68,35 @@ const ParkingSpotDetailsCard = ({ item, currentDate, type, navigation }) => {
                 <Text fontSize={16} fontWeight="bold">
                   Spot Features
                 </Text>
-                <Text>EV Charger</Text>
+                <Text>
+                  {item.paVehicleType === "1"
+                    ? "Standard"
+                    : type === "2"
+                    ? "Large"
+                    : type === "3"
+                    ? "Elecric car"
+                    : "Motorcycle"}
+                </Text>
               </VStack>
             </Box>
-            <Box borderBottomWidth={1} borderBottomColor="#FD6B36">
+            
+            <HStack justifyContent="space-between" alignItems="center">
+            <Box>
               <VStack space={2}>
                 <Text fontSize={16} fontWeight="bold">
                   Spot Owner
                 </Text>
                 <HStack space={4} mb={4}>
                   <Box py={2} px={3} borderRadius="20px" bg="#FD6B36">
-                    <Text color="white">JS</Text>
+                    <Text color="white">
+                      {item.upFirstName.charAt(0).toUpperCase()}
+                      {item.upLastName.charAt(0).toUpperCase()}
+                    </Text>
                   </Box>
                   <VStack>
-                    <Text fontSize={14}>John Smith</Text>
+                    <Text fontSize={14}>
+                      {item.upFirstName} {item.upLastName}
+                    </Text>
                     <Text color="grey" fontSize={13}>
                       Registered Jan 1, 2022
                     </Text>
@@ -75,19 +104,12 @@ const ParkingSpotDetailsCard = ({ item, currentDate, type, navigation }) => {
                 </HStack>
               </VStack>
             </Box>
-            <HStack justifyContent="space-between" alignItems="center">
-              <Box>
-                <Text fontSize={16} fontWeight="bold">
-                  Reservation For
-                </Text>
-                <HStack>
-                  <Text>Me, </Text>
-                  <Text> Benz, LMO2356</Text>
-                </HStack>
-              </Box>
               <Box>
                 <Button
+                  onPress={() => ChatHandler()}
+                  backgroundColor="#0CB183"
                   borderRadius="20px"
+                  mt="16px"
                   pl="1px"
                   startIcon={
                     <Icon
@@ -107,11 +129,11 @@ const ParkingSpotDetailsCard = ({ item, currentDate, type, navigation }) => {
           </VStack>
         </Box>
         <Button borderRadius="20px" backgroundColor="#FD6B36" mb={7}>
-          {(type = "inUse")
+          {type === "inUse"
             ? "LEAVE SPOT"
-            : (type = "upcoming")
+            : type === "upcoming"
             ? "CANCEL RESERVATION"
-            : (type = "expired")
+            : type === "expired"
             ? "DELETE"
             : ""}
         </Button>
