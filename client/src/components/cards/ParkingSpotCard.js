16b1912daa2b9267ed5ec912c360eddedae57422
Javier Gongora <jgongora@gmail.com>
2022-03-17 21:16:46 -0700

    Update ParkingSpotCard.js

--- client/src/components/cards/ParkingSpotCard.js
+++ client/src/components/cards/ParkingSpotCard.js
@@ -5,7 +5,6 @@ import {
   Divider,
   HStack,
   Icon,
-  Pressable,
   Text,
   VStack,
 } from "native-base";
@@ -30,6 +29,7 @@ const ParkingSpotCard = ({ item, currentDate, type, navigation }) => {
   };
 
   const hostDetailsHandler = () => {
+    console.log("type:", type);
     navigation.navigate("HostStack", {
       screen: "HostDetailsScreen",
       params: { item: item, currentDate: currentDate, type: type },
@@ -37,11 +37,12 @@ const ParkingSpotCard = ({ item, currentDate, type, navigation }) => {
   };
 
   const hostEditHandler = () => {
+    console.log("type:", type);
     navigation.navigate("HostStack", {
       screen: "HostEditScreen",
       params: { item: item, currentDate: currentDate, type: type },
     });
-  };
+  }; 
 
   const whoParksHandler = () => {
     console.log("type:", type);
@@ -80,123 +81,149 @@ const ParkingSpotCard = ({ item, currentDate, type, navigation }) => {
               return;
           }
         }}
-      >
-        <Box
-          mx="4"
-          my="1"
-          px="4"
-          py="2"
-          borderWidth="1"
-          rounded="2xl"
-          borderColor="coolGray.300"
-          borderRadius="20px"
-          backgroundColor="white"
-          shadow={4}
         >
-          <VStack space="4">
-            <HStack space="4">
-              <Box
-                width="39"
-                height="39"
-                rounded="full"
-                backgroundColor="#FD6B36"
-              >
-                <Center h="39">
-                  <Text color="white" fontSize="xl">
-                    {item.upFirstName.charAt(0).toUpperCase()}
-                    {item.upLastName.charAt(0).toUpperCase()}
-                  </Text>
-                </Center>
-              </Box>
-              <VStack space="1">
-                <Box>
-                  <Text fontSize="xl">Spot {item.idParkingSlot}</Text>
-                </Box>
-                <HStack>
-                  <Box width="79%">
-                    {item.paVehicleType == "1" && (
-                      <Box
-                        width="75"
-                        px="2"
-                        borderWidth="1"
-                        borderColor="#0CB183"
-                        rounded="full"
-                      >
-                        <Text flex="1" fontSize="13" color="#0CB183">
-                          Standard
-                        </Text>
-                      </Box>
-                    )}
-                    {item.paVehicleType == "2" && (
-                      <Box
-                        width="55"
-                        px="2"
-                        borderWidth="1"
-                        borderColor="#FD6B36"
-                        rounded="full"
-                      >
-                        <Text fontSize="13" color="#FD6B36">
-                          Large
-                        </Text>
-                      </Box>
-                    )}
-                    {item.paVehicleType == "3" && (
-                      <Box
-                        width="90"
-                        px="2"
-                        borderWidth="1"
-                        borderColor="#09A1C6"
-                        rounded="full"
-                      >
-                        <Text fontSize="13" color="#09A1C6">
-                          EV Charger
-                        </Text>
-                      </Box>
-                    )}
-                    {item.paVehicleType == "4" && (
-                      <Box
-                        width="87"
-                        px="2"
-                        borderWidth="1"
-                        borderColor="#DB7D16"
-                        rounded="full"
-                      >
-                        <Text fontSize="13" color="#DB7D16">
-                          Motorcycle
-                        </Text>
-                      </Box>
-                    )}
-                  </Box>
-                  <Box>
-                    <Button
-                      background="transparent"
-                      startIcon={
-                        <Icon
-                          color="black"
-                          size={5}
-                          ml={2}
-                          as={MaterialIcons}
-                          name="navigate-next"
-                        />
-                      }
-                    />
-                  </Box>
-                </HStack>
-              </VStack>
-            </HStack>
-            <HStack space="4" justifyContent="space-between">
-              <Box>
-                <Text>
-                  {currentDate}, {item.rsrv_start.slice(0, 5)} -{" "}
-                  {item.rsrv_end.slice(0, 5)}
+      <Box
+        mx="4"
+        my="1"
+        px="4"
+        py="2"
+        borderWidth="1"
+        rounded="2xl"
+        borderColor="coolGray.300"
+        borderRadius="20px"
+        backgroundColor="white"
+        shadow={4}
+      >
+        <VStack space="4">
+          <HStack space="4">
+            <Box
+              width="39"
+              height="39"
+              rounded="full"
+              backgroundColor="#FD6B36"
+            >
+              <Center h="39">
+                <Text color="white" fontSize="xl">
+                  {item.upFirstName.charAt(0).toUpperCase()}
+                  {item.upLastName.charAt(0).toUpperCase()}
                 </Text>
-              </Box>
+              </Center>
+            </Box>
+            <VStack space="1">
               <Box>
-                <Text>CAD ${item.paFee}</Text>
+                <Text fontSize="xl">Spot {item.idParkingSlot}</Text>
               </Box>
-            </HStack>
-          </VStack>
-        </Box>
+              <HStack>
+                <Box width="79%">
+                  {item.paVehicleType == "1" && (
+                    <Box
+                      width="75"
+                      px="2"
+                      borderWidth="1"
+                      borderColor="#0CB183"
+                      rounded="full"
+                    >
+                      <Text flex="1" fontSize="13" color="#0CB183">
+                        Standard
+                      </Text>
+                    </Box>
+                  )}
+                  {item.paVehicleType == "2" && (
+                    <Box
+                      width="55"
+                      px="2"
+                      borderWidth="1"
+                      borderColor="#FD6B36"
+                      rounded="full"
+                    >
+                      <Text fontSize="13" color="#FD6B36">
+                        Large
+                      </Text>
+                    </Box>
+                  )}
+                  {item.paVehicleType == "3" && (
+                    <Box
+                      width="90"
+                      px="2"
+                      borderWidth="1"
+                      borderColor="#09A1C6"
+                      rounded="full"
+                    >
+                      <Text fontSize="13" color="#09A1C6">
+                        EV Charger
+                      </Text>
+                    </Box>
+                  )}
+                  {item.paVehicleType == "4" && (
+                    <Box
+                      width="87"
+                      px="2"
+                      borderWidth="1"
+                      borderColor="#DB7D16"
+                      rounded="full"
+                    >
+                      <Text fontSize="13" color="#DB7D16">
+                        Motorcycle
+                      </Text>
+                    </Box>
+                  )}
+                </Box>
+                <Box>
+                  <Button
+                    onPress={() => {
+                      switch (type) {
+                        case "today":
+                        case "thisWeek":
+                        case "thisMonth":
+                          findParkingHandler();
+                          break;
+                        case "inUse":
+                        case "upcoming":
+                        case "expired":
+                          detailsHandler();
+                          break;
+                        case "hostReservation":
+                        case "hostArchive":
+                          hostDetailsHandler();
+                          break;
+                        case "hostSpot":
+                          hostEditHandler();
+                          break;
+                          case "searchResult":
+                          whoParksHandler();
+                          break;
+                        default:
+                         return;
+                      }
+                    }}
+                    background="transparent"
+                    startIcon={
+                      <Icon
+                        color="black"
+                        size={5}
+                        ml={2}
+                        as={MaterialIcons}
+                        name="navigate-next"
+                      />
+                    }
+                  />
+                </Box>
+              </HStack>
+            </VStack>
+          </HStack>
+          <HStack space="4" justifyContent="space-between">
+            <Box>
+              <Text>
+                {currentDate}, {item.rsrv_start.slice(0, 5)} -{" "}
+                {item.rsrv_end.slice(0, 5)}
+              </Text>
+            </Box>
+            <Box>
+              <Text>CAD ${item.paFee}</Text>
+            </Box>
+          </HStack>
+        </VStack>
+      </Box>
       </Pressable>
     </>
   );
