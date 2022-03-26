a08d8f88c19cb12a30f2aa8f308a539a198a9c5a
Doug Leung <84534062+dougieleung@users.noreply.github.com>
2022-03-26 11:57:24 -0700

    feat: post reservation success (#90)
    * feat: post reservation success
    
    * click parking spot card, goes straight to whopark
    
    * fix: fontsize to number

--- server/controllers/reservationControllers.js
+++ server/controllers/reservationControllers.js
@@ -3,64 +3,66 @@ const db = require("../config/config.js");
 
 // Create and Save a new reservation
 exports.createReservation = (req, res) => {
-    // Validate request
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-    }
-
-    // Create a reservation
-    const reservation = new Reservation({
-        rsvparkingslotid: req.body.rsvparkingslotid,
-        rsvvisitorid: req.body.rsvvisitorid,
-        rsvdtstart: req.body.rsvdtstart,
-        rsvdtend: req.body.rsvdtend,
-        rsvstatus: req.body.rsvstatus,
-        rsvtype: req.body.rsvtype,
-        rsvfee: req.body.rsvfee,
-        rsvcarplateno: req.body.rsvcarplateno,
-        rsvcarmodel: req.body.rsvcarmodel
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!",
     });
+  }
 
-    // Save Reservation in the database
-    Reservation.saveReservation(reservation, (err, data) => {
-        if (err)
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the reservation."
-            });
-        else res.send(data);
-    });
+  // Create a reservation
+  const reservation = new Reservation({
+    rsvparkingslotid: req.body.rsvparkingslotid,
+    rsvvisitorid: req.body.rsvvisitorid,
+    rsvdtstart: req.body.rsvdtstart,
+    rsvdtend: req.body.rsvdtend,
+    rsvstatus: req.body.rsvstatus,
+    rsvtype: req.body.rsvtype,
+    rsvfee: req.body.rsvfee,
+    rsvcarplateno: req.body.rsvcarplateno,
+    rsvcarmodel: req.body.rsvcarmodel,
+  });
+
+  // Save Reservation in the database
+  Reservation.saveReservation(reservation, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the reservation.",
+      });
+    else res.send(data);
+  });
 };
 
 exports.cancelReservation = (req, res) => {
-    // Validate request
-    if (!req.body) {
-        res.status(400).send({
-            message: "Content can not be empty!"
-        });
-    }
-
-    // Create a reservation
-    const reservationId = req.params.bookingid;
-
-    // Save Reservation in the database
-    Reservation.setReservationToCancel(reservationId, (err, data) => {
-        if (err)
-            res.status(500).send({
-                message: err.message || "Some error occurred while creating the reservation."
-            });
-        else res.send(data);
+  // Validate request
+  if (!req.body) {
+    res.status(400).send({
+      message: "Content can not be empty!",
     });
+  }
+
+  // Create a reservation
+  const reservationId = req.params.bookingid;
+
+  // Save Reservation in the database
+  Reservation.setReservationToCancel(reservationId, (err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the reservation.",
+      });
+    else res.send(data);
+  });
 };
 
 exports.findAll = (req, res) => {
-
-    Reservation.getAll((err, data) => {
-        if (err)
-            res.status(500).send({
-                message: err.message || "Some error occurred while retrieving reservations."
-            });
-        else res.send(data);
-    });
+  Reservation.getAll((err, data) => {
+    if (err)
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving reservations.",
+      });
+    else res.send(data);
+  });
 };
