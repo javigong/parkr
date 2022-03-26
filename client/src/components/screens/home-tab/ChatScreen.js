068e4ce7b204487bb516f7ef8c097e73d41161c4
Yulia <mail4yulia@yahoo.com>
2022-03-26 11:50:26 -0700

    liveChatScreen visibility testing

--- client/src/components/screens/home-tab/ChatScreen.js
+++ client/src/components/screens/home-tab/ChatScreen.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { Center, Text } from "native-base";
+import { SafeAreaView } from "react-native";
+import { Center, Heading, Text } from "native-base";
+import LiveChatScreen from "../chat-stack/LiveChatScreen";
 
-const ChatScreen = () => {
+const ChatScreen = ({ navigation }) => {
   return (
-    <Center>
-      <Center
-        bg="primary.400"
-        _text={{
-          color: "white",
-          fontWeight: "bold",
-        }}
-        height={200}
-        width={{
-          base: 200,
-          lg: 255,
-        }}
-      >
-        ChatScreen
-      </Center>
-    </Center>
+    // <Center>
+    //   <Center
+    //     bg="primary.400"
+    //     _text={{
+    //       color: "white",
+    //       fontWeight: "bold",
+    //     }}
+    //     height={200}
+    //     width={{
+    //       base: 200,
+    //       lg: 255,
+    //     }}
+    //   >
+    //     <Heading>Chat Screen</Heading>
+    //   </Center>
+    // </Center>
+    <SafeAreaView>
+      <LiveChatScreen />
+    </SafeAreaView>
   );
 };
 
