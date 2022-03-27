8d570af78622faf9d4308f4a5878d38ba3d4bf9f
Yulia <mail4yulia@yahoo.com>
2022-03-26 19:23:21 -0700

    paste NativeBase SwipeList View component into Chat Screen with Smaple data, add all dependencies, screen rendering successfully

--- client/src/components/screens/chat-stack/LiveChatScreen.js
+++ client/src/components/screens/chat-stack/LiveChatScreen.js
@@ -14,74 +14,73 @@ import { signOut } from "firebase/auth";
 import { auth, database } from "../../config/firebase";
 
 const LiveChatScreen = ({ navigation }) => {
-  //   const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([]);
 
-  //   const onSignOut = () => {
-  //     signOut(auth).catch((error) => console.log("Error logging out: ", error));
-  //   };
+  const onSignOut = () => {
+    signOut(auth).catch((error) => console.log("Error logging out: ", error));
+  };
 
-  //   useLayoutEffect(() => {
-  //     navigation.setOptions({
-  //       headerRight: () => (
-  //         <TouchableOpacity
-  //           style={{
-  //             marginRight: 10,
-  //           }}
-  //           onPress={onSignOut}
-  //         >
-  //           <Text>Logout</Text>
-  //         </TouchableOpacity>
-  //       ),
-  //     });
-  //   }, [navigation]);
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity
+          style={{
+            marginRight: 10,
+          }}
+          onPress={onSignOut}
+        >
+          <Text>Logout</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation]);
 
-  //   // useEffect(() => {
-  //   //   setMessages([
+  // useEffect(() => {
+  //   setMessages([
 
-  //   // }, []);
+  // }, []);
 
-  //   useLayoutEffect(() => {
-  //     const collectionRef = collection(database, "chats");
-  //     const q = query(collectionRef, orderBy("createdAt", "desc"));
+  useLayoutEffect(() => {
+    const collectionRef = collection(database, "chats");
+    const q = query(collectionRef, orderBy("createdAt", "desc"));
 
-  //     const unsubscribe = onSnapshot(q, (querySnapshot) => {
-  //       setMessages(
-  //         querySnapshot.docs.map((doc) => ({
-  //           _id: doc.data()._id,
-  //           createdAt: doc.data().createdAt.toDate(),
-  //           text: doc.data().text,
-  //           user: doc.data().user,
-  //         }))
-  //       );
-  //     });
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setMessages(
+        querySnapshot.docs.map((doc) => ({
+          _id: doc.data()._id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user,
+        }))
+      );
+    });
 
-  //     return unsubscribe;
-  //   });
+    return unsubscribe;
+  });
 
-  //   const onSend = useCallback((messages = []) => {
-  //     setMessages((previousMessages) =>
-  //       GiftedChat.append(previousMessages, messages)
-  //     );
-  //     const { _id, createdAt, text, user } = messages[0];
-  //     addDoc(collection(database, "chats"), {
-  //       _id,
-  //       createdAt,
-  //       text,
-  //       user,
-  //     });
-  //   }, []);
+  const onSend = useCallback((messages = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+    const { _id, createdAt, text, user } = messages[0];
+    addDoc(collection(database, "chats"), {
+      _id,
+      createdAt,
+      text,
+      user,
+    });
+  }, []);
 
   return (
-    // <GiftedChat
-    //   messages={messages}
-    //   showAvatarForEveryMessage={true}
-    //   onSend={(messages) => onSend(messages)}
-    //   user={{
-    //     _id: auth?.currentUser?.email,
-    //     avatar: "https://i.pravatar.cc/300",
-    //   }}
-    // />
-    <Heading>Live Chat Screen</Heading>
+    <GiftedChat
+      messages={messages}
+      showAvatarForEveryMessage={true}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: auth?.currentUser?.email,
+        avatar: "https://i.pravatar.cc/300",
+      }}
+    />
   );
 };
 
