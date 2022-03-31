b7a455e230a000a03d1ac8b99759921c7df8653b
Javier Gongora <jgongora@gmail.com>
2022-03-31 12:23:20 -0700

    Update ParkingSpotList.js

--- client/src/components/lists/ParkingSpotList.js
+++ client/src/components/lists/ParkingSpotList.js
@@ -1,5 +1,6 @@
 import { FlatList } from "native-base";
 import React from "react";
+import HostSpotCard from "../cards/HostSpotCard";
 import ParkingSpotCard from "../cards/ParkingSpotCard";
 
 const ParkingSpotList = ({
@@ -11,22 +12,24 @@ const ParkingSpotList = ({
   endDate,
 }) => {
   return (
-    <FlatList
-      my="2"
-      data={data}
-      renderItem={({ item }) => (
-        <ParkingSpotCard
-          item={item}
-          currentDate={currentDate}
-          startDate={startDate}
-          endDate={endDate}
-          type={type}
-          navigation={navigation}
-        />
-      )}
-      keyExtractor={(item) => item.idParkingSlot.toString()}
-      showsVerticalScrollIndicator={true}
-    />
+    <>
+      <FlatList
+        my="2"
+        data={data}
+        renderItem={({ item }) => (
+          <ParkingSpotCard
+            item={item}
+            currentDate={currentDate}
+            startDate={startDate}
+            endDate={endDate}
+            type={type}
+            navigation={navigation}
+          />
+        )}
+        keyExtractor={(item) => item.idParkingSlot.toString()}
+        showsVerticalScrollIndicator={true}
+      />
+    </>
   );
 };
 
